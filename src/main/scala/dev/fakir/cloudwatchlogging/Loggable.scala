81de100398e655245f816c81eaf443e4acad1ab1
fakirAyoub <ayoub@fakir.dev>
2020-10-01 03:31:33 +0200

    info and error OK - WIP

--- src/main/scala/dev/fakir/cloudwatchlogging/Loggable.scala
+++ src/main/scala/dev/fakir/cloudwatchlogging/Loggable.scala
@@ -1,13 +1,14 @@
 package dev.fakir.cloudwatchlogging
 
 import com.amazonaws.services.logs.AWSLogs
-import com.amazonaws.services.logs.model.PutLogEventsRequest
+import com.amazonaws.services.logs.model.{InvalidSequenceTokenException, PutLogEventsRequest}
 import dev.fakir.cloudwatchlogging.models.CloudWatchLogsProperties
 import dev.fakir.cloudwatchlogging.utils.{CloudWatchEnv, CloudWatchEvents}
 import org.slf4j.LoggerFactory
 import CloudWatchEnv._
 import CloudWatchEvents._
-import zio.{UIO, ZIO}
+import zio.clock.Clock
+import zio.{Schedule, Task, UIO, ZIO}
 
 trait Loggable {
   def region: String
@@ -18,15 +19,25 @@ trait Loggable {
   val cloudWatchError: PutLogEventsRequest = getRequestEvent(CloudWatchLogsProperties(logGroup, errorLogStream))
   val cloudWatchInfo: PutLogEventsRequest = getRequestEvent(CloudWatchLogsProperties(logGroup, infoLogStream))
   @transient private lazy val logger = LoggerFactory.getLogger(getClass)
-  protected def info[T](msg: Map[String, T]): UIO[Unit] = {
-    logger.info(msg.toString)
-    val infoEvent = buildEvent(msg)
-    UIO(CloudWatchEvents.writeEvents(Seq(infoEvent), cloudWatchInfo))
+  protected def info[T](msg: Map[String, T]): ZIO[Any with Clock, Throwable, Unit] = {
+    for {
+      _           <- UIO(logger.info(msg.toString))
+      infoEvent   <- Task(buildEvent(msg))
+      writeEvent  <- Task(CloudWatchEvents.writeEvents(Seq(infoEvent), cloudWatchInfo))
+      _           <- writeEvent.catchSome {
+        case e: InvalidSequenceTokenException => writeEvent.retry(Schedule.recurs(5))
+      }
+    } yield ()
   }
-  protected def error[T](msg: Map[String, T]): Unit = {
-    logger.error(msg.toString())
-    val infoEvent = buildEvent(msg)
-    UIO(writeEvents(Seq(infoEvent), cloudWatchError))
+  protected def error[T](msg: Map[String, T]): ZIO[Any with Clock, Throwable, Unit] = {
+    for {
+      _           <- UIO(logger.error(msg.toString))
+      infoEvent   <- Task(buildEvent(msg))
+      writeEvent  <- Task(CloudWatchEvents.writeEvents(Seq(infoEvent), cloudWatchError))
+      _           <- writeEvent.catchSome {
+        case e: InvalidSequenceTokenException => writeEvent.retry(Schedule.recurs(5))
+      }
+    } yield ()
   }
 
   protected def info(msg: String): Unit = {
